551be45e2b20083c56787afb8b96a3d455f0b3f6
lab-4 <lab-4@DESKTOP-NGHDCS3>
2023-10-30 01:51:41 +0000

    Membuat apk

---
+++ src/aplikasi/penjualan/netbeans/k07/AplikasiPenjualanNetbeansK07.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi.penjualan.netbeans.k07;
+
+/**
+ *
+ * @author lab-4
+ */
+public class AplikasiPenjualanNetbeansK07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
