9231eebd0ec6e55a06b077a216a797b8f94fdfa4
hafifah0608@gmail.com <tarwini18@gmail.com>
2023-10-30 09:23:04 +0700

    tambah koneksi-subhan

---
+++ src/aplikasi/penjualan/netbeans/k07/Koneksi.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi.penjualan.netbeans.k07;
+
+/**
+ *
+ * @author SMKI Utama5
+ */
+public class Koneksi {
+    
+}
