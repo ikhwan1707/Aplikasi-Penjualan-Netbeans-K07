26811d3f00ef5bb7de20d070eaeb265fc4fd3e7f
NAUFAL YUSTIN AGIVA <NAUFAL YUSTIN AGIVA@LAPTOP-JIDLH3T6>
2023-10-30 21:04:09 +0700

    Desain Table Jenis - Naufal

--- src/aplikasi/penjualan/netbeans/k07/tableJenis.java
+++ src/aplikasi/penjualan/netbeans/k07/tableJenis.java
@@ -26,18 +26,78 @@ public class tableJenis extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtkode = new javax.swing.JTextField();
+        txtjenisbarang = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbljenis = new javax.swing.JTable();
+        btnnew = new javax.swing.JButton();
+        btnsave = new javax.swing.JButton();
+        btnupdate = new javax.swing.JButton();
+        btndelete = new javax.swing.JButton();
+        btncancel = new javax.swing.JButton();
+        btnclose = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel1.setToolTipText("");
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Entri Data Jenis Barang");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 0, -1, 50));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 480, 50));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("Kode Jenis");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setText("Jenis Barang");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, -1, -1));
+        getContentPane().add(txtkode, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 70, -1, -1));
+        getContentPane().add(txtjenisbarang, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 110, 350, -1));
+
+        tbljenis.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tbljenis);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 440, 240));
+
+        btnnew.setText("Add New");
+        getContentPane().add(btnnew, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 390, 90, -1));
+
+        btnsave.setText("Save");
+        getContentPane().add(btnsave, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 390, -1, -1));
+
+        btnupdate.setText("Update");
+        getContentPane().add(btnupdate, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 390, -1, -1));
+
+        btndelete.setText("Delete");
+        getContentPane().add(btndelete, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 390, -1, -1));
+
+        btncancel.setText("Cancel");
+        getContentPane().add(btncancel, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 390, -1, -1));
+
+        btnclose.setText("Close");
+        getContentPane().add(btnclose, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 390, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +138,19 @@ public class tableJenis extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btncancel;
+    private javax.swing.JButton btnclose;
+    private javax.swing.JButton btndelete;
+    private javax.swing.JButton btnnew;
+    private javax.swing.JButton btnsave;
+    private javax.swing.JButton btnupdate;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbljenis;
+    private javax.swing.JTextField txtjenisbarang;
+    private javax.swing.JTextField txtkode;
     // End of variables declaration//GEN-END:variables
 }
