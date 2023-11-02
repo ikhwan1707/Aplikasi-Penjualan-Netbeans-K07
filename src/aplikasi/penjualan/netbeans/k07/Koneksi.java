0878ca207272c926320af0da1729ef6a322e5bdc
firmanajh <firmansyahajah06@gmail.com>
2023-11-02 09:55:11 +0700

    koneksi

--- src/aplikasi/penjualan/netbeans/k07/Koneksi.java
+++ src/aplikasi/penjualan/netbeans/k07/Koneksi.java
@@ -4,10 +4,33 @@
  */
 package aplikasi.penjualan.netbeans.k07;
 
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author SMKI Utama5
  */
 public class Koneksi {
+    public static Connection koneksi;
+    public static Connection getKoneksi(){
     
+    if (koneksi == null)
+    {
+        try {
+            String url = "jdbc:mysql://localhost:3306/db_penjualan_barang_pas_xiia";
+            String user = "root";
+            String password = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            koneksi = DriverManager.getConnection(url, user, password);
+            System.out.print("Berhasil Membuat Koneksi");
+        }catch(SQLException t){
+            System.out.println("Error Membuat Koneksi");
+        }
+    }
+    return koneksi;
+    }
 }
