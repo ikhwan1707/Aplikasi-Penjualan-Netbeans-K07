d029245b7658d0a9892b3db2d085e1cfc9aea13c
ilumni <ilumni.dev@gmail.com>
2023-11-08 17:42:04 +0700

    membuat Create table jenis

---
+++ src/aplikasi/penjualan/netbeans/k07/s.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi.penjualan.netbeans.k07;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author RnD
+ */
+class s {
+
+    static ResultSet executeQuery(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
